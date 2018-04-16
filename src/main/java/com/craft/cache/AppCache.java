7148219874ee081390f2f31b8bb33ed713220ed5
Atul Gupta <atul.bits180@gmail.com>
2018-04-16 15:13:34 -0700

    commit-10:additional changes

--- src/main/java/com/craft/cache/AppCache.java
+++ src/main/java/com/craft/cache/AppCache.java
@@ -3,18 +3,26 @@ package com.craft.cache;
 import com.craft.entity.Entity;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class AppCache {
     private static final Logger log = LoggerFactory.getLogger(AppCache.class);
     private Map<String, Entity> _cache ;
 
+    @Value("${cache.size}")
+    private int MAX_CACHE_SIZE;
+
     public void add(String k, Entity v){
-        _cache.put(k,v);
+        /*if(_cache.size() == MAX_CACHE_SIZE && _cache.containsKey(k)){
+            // put a better implementation here
+            _cache.remove();
+        }*/
+        _cache.put(k, v);
     }
 
     public Entity get(String k){
@@ -25,7 +33,7 @@ public class AppCache {
     // cleans the existing cache and initializes new one
     public void initialize(){
         _cache = null;
-        _cache = new ConcurrentHashMap<>();
+        _cache = new LinkedHashMap<>();
     }
 
     public int size(){
