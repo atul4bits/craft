33c4f20374cb1d7612f5eff7d0e70bdf84dec509
Atul Gupta <atul.bits180@gmail.com>
2018-04-15 23:30:09 -0700

    commit-7:adding test cases

--- src/main/java/com/craft/controller/CacheRestController.java
+++ src/main/java/com/craft/controller/CacheRestController.java
@@ -15,6 +15,8 @@ import java.util.Date;
 @RestController
 public class CacheRestController {
 
+    private final String CACHE_NOT_READY =  "Cache is being loaded, please wait for few seconds..";
+
     @Autowired
     AppCache appCache;
 
@@ -26,7 +28,7 @@ public class CacheRestController {
     // as this will be called before invoking this method
     public Object getEntity(@RequestParam(value="key") String key) {
         if(loadXML.isCacheLoading()){
-            return "Cache is being loaded, hit in few seconds";
+            return CACHE_NOT_READY;
         }
         Entity e = appCache.get(key);
         return  (e  == null) ? "Key: " + key + " doesn't exist": e ;
@@ -37,7 +39,7 @@ public class CacheRestController {
     @RequestMapping(method = RequestMethod.POST, path = "/addEntity" ,produces = "application/json; charset=utf-8")
     public Object addEntity(@RequestParam(value="key")  String key , @RequestParam(value="val") String val ) {
         if(loadXML.isCacheLoading()){
-            return "Cache is being loaded, hit in few seconds";
+            return CACHE_NOT_READY;
         }
         Entity entity = new Entity(key,val,new Date());
         appCache.add(key,entity);
--- src/test/java/com/craft/CraftApplicationTests.java
+++ src/test/java/com/craft/CraftApplicationTests.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Calendar;
 
@@ -19,10 +19,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 public class CraftApplicationTests {
-
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
 
 	@Autowired
 	private CacheRestController cacheRestController;
@@ -40,31 +36,25 @@ public class CraftApplicationTests {
 	    assertThat(loadXML).isNotNull();
 	}
 
-	@Test
-    public void testsForAppCache(){
-	    assertThat(appCache.size() == 5);
-	    assertThat(appCache.get("t1").getValue().equals("val1"));
-	    appCache.add("T6", new Entity("T6", "val6", Calendar.getInstance().getTime()));
-	    assertThat(appCache.size() == 6);
+    @Test
+    @DependsOn("testsForAppCache")
+    public void testsForLoadXML(){
+        assertThat(loadXML.getFilePath()).isEqualTo("./cache_data_test.xml");
+        assertThat(appCache.size()).isEqualTo(6);
+        loadXML.loadXMLData();
+        assertThat(appCache.size()).isEqualTo(5);
+
     }
 
-    @Test
-    public void testsForLoadXML(){
-        assertThat(loadXML.getFilePath().equals("./cache_data_test.xml"));
 
-	}
+	@Test
+    public void testsForAppCache(){
+        assertThat(appCache.size()).isEqualTo(5);
+	    assertThat(appCache.get("t1").getValue()).isNotNull();
+
+	    appCache.add("T6", new Entity("T6", "val6", Calendar.getInstance().getTime()));
+        assertThat(appCache.size()).isGreaterThan(5);
+    }
 
 
-
-/*    @Before
-    public void setup() throws Exception {
-	    loadXML.loadXMLData();
-    }*/
-
-	/*
-    @Test
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello World")));
-    }*/
 }
